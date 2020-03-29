105156fcbae3521256bcb63ce46850ccede46917
Harsimran Dhoofar <sam.dhoofar@gmail.com>
2020-03-29 14:56:34 -0400

    Front end works with Drag and drop functionality and getting data dynamically from classes
    Front end works with Drag and drop functionality and getting data dynamically from classes

--- kanbanClone/src/app/pages/main-view/main-view.component.ts
+++ kanbanClone/src/app/pages/main-view/main-view.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { Board } from 'src/app/models/board.model';
+import { Column } from 'src/app/models/column.model';
 @Component({
   selector: 'app-main-view',
   templateUrl: './main-view.component.html',
@@ -8,8 +10,44 @@ import { Component, OnInit } from '@angular/core';
 export class MainViewComponent implements OnInit {
 
   constructor() { }
+board:Board =new Board('Test Board',[
+  new Column('Ideas',[
+          "Some random Idea",
+          "This is an other Random Idea",
+          "Build an Awsome Application"
+  ]),
+   new Column('Research',[
+            "This is Research",
+            "bobobo",
+            "lol"
+  ]),
+   new Column('ToDo',[
+    "Get to work",
+    "Pick up groceries",
+    "Go home",
+    "Fall asleep"
+   ]),
+   new Column('Done',[
+    "Get up",
+    "Brush teeth",
+    "Take a shower",
+    "Check e-mail",
+    "Walk dog"
+   ])
+  ]);
 
   ngOnInit(): void {
   }
+  
 
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
+  }
 }
